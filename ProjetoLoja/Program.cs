c99f278b57b12ebd69d46392ff63f3405680ffe5
MixhelCastro <castromichel694@gmail.com>
2025-08-20 08:40:13 -0300

    Adicionando comandos para registrar os serviços se seção.

--- ProjetoLoja/Program.cs
+++ ProjetoLoja/Program.cs
@@ -2,6 +2,17 @@ using ProjetoLoja.Repositorios;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 1. REGISTRAR OS SERVIÇOS DE SESSÃO
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Tempo de inatividade da sessão (pode ajustar)
+    options.Cookie.HttpOnly = true; // Impede que o cookie seja acessado por scripts do lado do cliente
+    options.Cookie.IsEssential = true; // Torna o cookie de sessão essencial para a funcionalidade do aplicativo
+});
+
+// REGISTRAR A CONNECTION STRING COMO UM SERVIÇO STRING AQUI
+builder.Services.AddSingleton<string>(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
