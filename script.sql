0db9b778415b3c498f86aaaa30999e942a2abdd8
MixhelCastro <castromichel694@gmail.com>
2025-08-20 08:30:17 -0300

    Adicionando script.sql, o banco de dados no projeto.

---
+++ script.sql
@@ -0,0 +1,36 @@
+-- criando banco de dados
+create database EcomLoja;
+-- usanado EcomLoja
+use EcomLoja;
+-- criando tabelas
+create table Produtos (
+id int primary key auto_increment,
+Nome varchar(40),
+Preco decimal(10.2),
+ImageUrl varchar(255),
+Estoque int
+);
+
+create table pedido(
+Id int primary key auto_increment ,
+DataPedido datetime,
+Total decimal(10,2),
+Status varchar(50),
+Endereco varchar(100),
+FormaPagamento varchar(100),
+Frete decimal (10,2)
+);
+
+create table itemPedido(
+Id int primary key auto_increment ,
+PedidoId int,
+ProdutoId int,
+Quantidade int,
+PrecoUnitario decimal(10,2)
+);
+
+-- CONSULTANDO AS TABELAS DO BANCO
+
+select * from produtos;
+select * from pedido;
+select * from itemPedido;
