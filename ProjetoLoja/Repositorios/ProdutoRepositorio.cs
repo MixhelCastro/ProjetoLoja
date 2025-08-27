0f19a8eae505c20bc73f3aeed76d7502091d4b45
MixhelCastro <castromichel694@gmail.com>
2025-08-27 08:20:03 -0300

    Criando conexão com banco de dados na parte de produtosRepositorio.

--- ProjetoLoja/Repositorios/ProdutoRepositorio.cs
+++ ProjetoLoja/Repositorios/ProdutoRepositorio.cs
@@ -1,13 +1,24 @@
-﻿namespace ProjetoLoja.Repositorios
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using ProjetoLoja.Models;
+
+namespace ProjetoLoja.Repositorios
 {
     public class ProdutoRepositorio
     {
         // DECLARANDO STRING DE CONEXÃO 
         private readonly string _connectionString;
-
+     
         public ProdutoRepositorio(string connectionString)
         {
             _connectionString = connectionString;
+        }
+        public async Task<IEnumerable<Produto>> TodosProdutos()
+        {
+            using var connection = new MySqlConnection(_connectionString);
+            var sql = "SELECT Id, Nome, Descricao, Preco, ImageUrl, Estoque FROM Produtos";
+            return await connection.QueryAsync<Produto>(sql);
+            // ORM, mapeamento relacional de banco de dados
         }
     }
 }
