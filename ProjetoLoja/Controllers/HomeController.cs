5a2b3d4cee59a26fd30d56f0c8ce4cec65405fd6
MixhelCastro <castromichel694@gmail.com>
2025-09-03 08:33:09 -0300

    Adicionando exibição para produtos.

--- ProjetoLoja/Controllers/HomeController.cs
+++ ProjetoLoja/Controllers/HomeController.cs
@@ -11,9 +11,10 @@ namespace ProjetoLoja.Controllers
 
         private readonly ProdutoRepositorio _produtoRepositorio;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ProdutoRepositorio produtoRepositorio)
         {
             _logger = logger;
+            _produtoRepositorio = produtoRepositorio;
         }
 
         public async Task<IActionResult> Index()
